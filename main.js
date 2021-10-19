4cb2c76389b99bebc667ea6328a2c4ffcaba816d
rad-ian <janek.radlinski@gmail.com>
2021-10-19 20:56:44 +0200

    make it calculate!

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 window.onload = Init;
-
+let number1 = "0"
 let isFirstDigitClick = true
+let lastOperatorClicked = null
 
 const displayElement = document.getElementById("display")
 
 function onButtonClick(event) {
     console.log(event.target.innerText)
+}
+
+function onEqualButtonClick(event) {
+    const result = eval(number1 + lastOperatorClicked + displayElement.innerText)
+    console.log(result)
+    displayElement.innerText = result
+}
+
+function onOperatorButtonClick(event) {
+    lastOperatorClicked = event.target.id
+    console.log(lastOperatorClicked, "operatorkliknięty")
+    number1 = displayElement.innerText
+    isFirstDigitClick = true
 }
 
 function onDigitButtonClick(event) {
@@ -17,9 +31,6 @@ function onDigitButtonClick(event) {
     } else {
         displayElement.innerText = displayElement.innerText + event.target.innerText
     }
-
-
-
 }
 
 function Init(event) {
@@ -34,4 +45,14 @@ function Init(event) {
         const button = digitButtons[i]
         button.onclick = onDigitButtonClick
     }
+
+    const operatorButtons = document.getElementsByClassName("operatorButton")
+    for (let i = 0; i < operatorButtons.length; i++) {
+        const operatorButton = operatorButtons[i]
+        operatorButton.onclick = onOperatorButtonClick
+    }
+
+    const equalButton = document.getElementById("equal")
+    equalButton.onclick = onEqualButtonClick
 }
+
